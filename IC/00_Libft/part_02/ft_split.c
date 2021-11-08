af786945121fb36960ecc51ad16f38dba961ea55
jrim <comfortindex@naver.com>
2021-11-08 19:32:16 +0900

    Add libft header and prototype

--- IC/00_Libft/part_02/ft_split.c
+++ IC/00_Libft/part_02/ft_split.c
@@ -1,84 +1,64 @@
-#include <stdlib.h>
+#include "libft.h"
 
-int		count_str(char *str, char *charset);
-void	make_strs(char **strs, int idx, char **ptr, char *charset);
-int		is_sep(char ch, char *charset);
+int		count_str(char const *s, char c);
+void	make_strs(char **strs, int idx, char **ptr, char c);
 
-char	**ft_split(char *str, char *charset)
+char    **ft_split(char const *s, char c)
 {
 	char	**strs;
 	char	*ptr;
 	int		idx;
 	int		str_cnt;
 
-	ptr = str;
-	str_cnt = count_str(ptr, charset) + 1;
+	ptr = s;
+	str_cnt = count_str(s, c) + 1;
 	strs = (char **)malloc(str_cnt * sizeof(char *));
 	if (!(strs))
 		return (0);
 	idx = 0;
 	while (idx < str_cnt - 1 && *ptr != '\0')
 	{
-		while (is_sep(*ptr, charset) == 1 && *ptr != '\0')
+		while (*ptr == c && *ptr != '\0')
 			ptr++;
-		make_strs(strs, idx, &ptr, charset);
+		make_strs(strs, idx, &ptr, c);
 		idx++;
 	}
 	strs[idx] = 0;
 	return (strs);
 }
 
-void	make_strs(char **strs, int idx, char **ptr, char *charset)
+int	count_str(char const *s, char c)
+{
+	int	idx;
+	int	cnt;
+
+	idx = 0;
+	cnt = 0;
+	while (s[idx] != '\0')
+	{
+		if (s[idx] == c)
+			cnt++;
+		idx++;
+	}
+	return (cnt);
+}
+
+void	make_strs(char **strs, int idx, char **ptr, char c)
 {
 	int	len;
 
 	len = 0;
-	while (is_sep(*(*ptr + len), charset) == 0 && *(*ptr + len) != '\0')
+	while (*(*ptr + len) == c && *(*ptr + len) != '\0')
 		len++;
 	strs[idx] = (char *)malloc((len + 1) * sizeof(char));
 	if (!(strs[idx]))
 		return ;
 	len = 0;
-	while (is_sep(**ptr, charset) == 0 && **ptr != '\0')
+	while (**ptr == c && **ptr != '\0')
 	{
 		strs[idx][len] = **(ptr);
 		len++;
 		(*ptr)++;
 	}
 	strs[idx][len] = '\0';
-}
-
-int	count_str(char *ptr, char *charset)
-{
-	int	cnt;
-	int	flag;
-
-	cnt = 0;
-	flag = 1;
-	while (*ptr != '\0')
-	{
-		if (is_sep(*ptr, charset) == 1 && flag == 0)
-			flag = 1;
-		else if (is_sep(*ptr, charset) == 0 && flag == 1)
-		{
-			flag = 0;
-			cnt++;
-		}
-		ptr++;
-	}
-	return (cnt);
-}
-
-int	is_sep(char ch, char *charset)
-{
-	int	i;
-
-	i = 0;
-	while (charset[i] != '\0')
-	{
-		if (ch == charset[i])
-			return (1);
-		i++;
-	}
-	return (0);
 }
--- IC/00_Libft/part_02/ft_strjoin.c
+++ IC/00_Libft/part_02/ft_strjoin.c
@@ -1,76 +1,32 @@
-#include <stdlib.h>
+#include "libft.h"
 
-int		ft_strlen(char *str);
-char	*ft_strcat(char *dest, char *src);
-char	*ft_malloc_str(int size, char **strs, char **new_str, char *sep);
+char	*ft_strcat(char *dst, char *src);
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+char    *ft_strjoin(char const *s1, char const *s2)
 {
 	int		idx;
+	int		total_len;
 	char	*new_str;
 
-	if (size == 0)
-	{
-		new_str = (char *)malloc(1 * sizeof(char));
-		return (new_str);
-	}
-	else
-		new_str = ft_malloc_str(size, strs, &new_str, sep);
 	idx = 0;
+	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	new_str = (char *)malloc(total_len * sizeof(char));
 	new_str[0] = '\0';
-	while (idx < size)
-	{
-		new_str = ft_strcat(new_str, strs[idx]);
-		if (idx < size - 1)
-			new_str = ft_strcat(new_str, sep);
-		idx++;
-	}
+	ft_strcat(s1, s2);
 	return (new_str);
 }
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strcat(char *dst, char *src)
 {
-	unsigned int	i;
-	unsigned int	j;
+	int	i;
+	int	j;
 
 	i = 0;
 	j = 0;
-	while (dest[i] != '\0')
+	while (dst[i] != '\0')
 		i++;
 	while (src[j] != '\0')
-		dest[i++] = src[j++];
-	dest[i] = '\0';
-	return (dest);
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_malloc_str(int size, char **strs, char **new_str, char *sep)
-{
-	int	total_len;
-	int	sep_len;
-	int	strs_len;
-	int	i;
-
-	strs_len = 0;
-	sep_len = ft_strlen(sep);
-	i = 0;
-	while (i < size)
-	{
-		strs_len += ft_strlen(strs[i]);
-		i++;
-	}
-	total_len = strs_len + sep_len * (size - 1) + 1;
-	*new_str = (char *)malloc(total_len * sizeof(char));
-	if (!(*new_str))
-		return (0);
-	return (*new_str);
+		dst[i++] = src[j++];
+	dst[i] = '\0';
+	return (dst);
 }
