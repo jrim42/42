e9fb14d1c43e063ef570c23e28ec3e990cff9cc0
jrim <comfortindex@naver.com>
2021-11-09 14:19:52 +0900

    Solve iteri and mapi

--- IC/00_Libft/part_02/ft_striteri.c
+++ IC/00_Libft/part_02/ft_striteri.c
@@ -2,5 +2,14 @@
 
 void    ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    
+    unsigned int    idx;
+
+    if (s == 0 || f == 0)
+        return (0);
+    idx = 0;
+    while (s[idx] != '\0')
+    {
+        f(idx, &s[idx]);
+        idx++;
+    }
 }
--- IC/00_Libft/part_02/ft_strmapi.c
+++ IC/00_Libft/part_02/ft_strmapi.c
@@ -2,5 +2,22 @@
 
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    
+    unsigned int    s_len;   
+    unsigned int    idx;
+    char            *new_str;
+
+    if (s == 0 || f == 0)
+        return (0);
+    s_len = ft_strlen(s);
+    new_str = (char *)malloc((s_len + 1) * sizeof(char));
+    if (!new_str)
+        return (0);
+    idx = 0;
+    while (idx < s_len)
+    {
+        new_str[idx] = f(idx, s[idx]);
+        idx++;
+    }
+    new_str[idx] = '\0';
+    return (new_str);
 }
