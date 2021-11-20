470c4d14f3b2ff7c29a4cf26eae774bb6cd6e319
jrim <comfortindex@naver.com>
2021-11-20 16:37:51 +0900

    Create files and add header

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 15:15:35 by jrim              #+#    #+#             */
-/*   Updated: 2021/11/20 15:15:37 by jrim             ###   ########.fr       */
+/*   Updated: 2021/11/20 15:37:17 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char    *get_next_line(int fd);
+
+#endif
