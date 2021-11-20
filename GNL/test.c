77565a03881cdc8ed47c85df0b0899440eb85877
jrim <comfortindex@naver.com>
2021-11-20 18:10:31 +0900

    Create GNL dir

---
+++ GNL/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 12
+
+int	main(void)
+{
+	char	buf[BUFFER_SIZE];
+	int		fd;
+	int		byte;
+
+	fd = open("test", O_RDONLY);
+	byte = read(fd, buf, BUFFER_SIZE);
+	printf("fd : %d\n", fd);
+	printf("byte : %d\n", byte);
+	printf("buf : %s\n", buf);
+}
