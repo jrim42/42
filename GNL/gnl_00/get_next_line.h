c2e0a41dc1c6c3309e1d40ef0fae04bf20be5e1a
jrim <comfortindex@naver.com>
2021-11-23 21:27:53 +0900

    Append header

--- GNL/gnl_00/get_next_line.h
+++ GNL/gnl_00/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 15:15:35 by jrim              #+#    #+#             */
-/*   Updated: 2021/11/23 15:59:27 by jrim             ###   ########.fr       */
+/*   Updated: 2021/11/23 20:06:17 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@
 # endif
 
 char	*get_next_line(int fd);
+char	*eol_trim(char *depot);
+char	*depot_trim(char *depot);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
