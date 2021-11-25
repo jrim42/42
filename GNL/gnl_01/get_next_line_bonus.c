2847e2e587607e3ef92ceb4a6bfcf99fc58ca44e
Jisu Rim <jrim@c6r7s1.42seoul.kr>
2021-11-25 16:00:05 +0900

    Solve gnl with bonus part

--- GNL/gnl_01/get_next_line_bonus.c
+++ GNL/gnl_01/get_next_line_bonus.c
@@ -12,29 +12,29 @@
 
 #include "get_next_line_bonus.h"
 
-char	*fill_depot(int fd, char **depot);
-char	*trim_eol(int fd, char **depot);
-char	*trim_depot(int fd, char **depot);
+char	*fill_depot(int fd, char *depot_fd);
+char	*trim_eol(char *depot_fd);
+char	*trim_depot(char *depot_fd);
 
 char	*get_next_line(int fd)
 {
 	static char	*depot[OPEN_MAX];
 	char		*one_line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > OPEN_MAX - 1)
 		return (0);
-	depot[fd] = fill_depot(fd, &depot[fd]);
+	depot[fd] = fill_depot(fd, depot[fd]);
 	if (!depot[fd] || !(*(depot[fd])))
 	{
 		free(depot[fd]);
 		return (0);
 	}
-	one_line = trim_eol(fd, &depot[fd]);
-	depot[fd] = trim_depot(fd, &depot[fd]);
+	one_line = trim_eol(depot[fd]);
+	depot[fd] = trim_depot(depot[fd]);
 	return (one_line);
 }
 
-char	*fill_depot(int fd, char **depot)
+char	*fill_depot(int fd, char *depot_fd)
 {
 	char	*buf;
 	int		byte;
@@ -43,7 +43,7 @@ char	*fill_depot(int fd, char **depot)
 	if (!buf)
 		return (0);
 	byte = 1;
-	while (byte > 0 && ft_strchr(depot[fd], '\n') == 0)
+	while (byte > 0 && ft_strchr(depot_fd, '\n') == 0)
 	{
 		byte = read(fd, buf, BUFFER_SIZE);
 		if (byte == -1)
@@ -52,38 +52,38 @@ char	*fill_depot(int fd, char **depot)
 			return (0);
 		}
 		buf[byte] = '\0';
-		depot[fd] = ft_strjoin(depot[fd], buf);
+		depot_fd = ft_strjoin(depot_fd, buf);
 	}
 	free(buf);
-	return (depot[fd]);
+	return (depot_fd);
 }
 
-char	*trim_eol(int fd, char **depot)
+char	*trim_eol(char *depot_fd)
 {
 	int	eol_idx;
 
-	if (ft_strchr(depot[fd], '\n') == 0)
-		return (depot[fd]);
-	eol_idx = (int)(ft_strchr(depot[fd], '\n') - depot[fd]);
-	return (ft_strndup(depot[fd], eol_idx + 1));
+	if (ft_strchr(depot_fd, '\n') == 0)
+		return (depot_fd);
+	eol_idx = (int)(ft_strchr(depot_fd, '\n') - depot_fd);
+	return (ft_strndup(depot_fd, eol_idx + 1));
 }
 
-char	*trim_depot(int fd, char **depot)
+char	*trim_depot(char *depot_fd)
 {
 	int		eol_idx;
 	int		trimmed_len;
 	char	*new_depot;
 
-	if (ft_strchr(depot[fd], '\n') == 0)
+	if (ft_strchr(depot_fd, '\n') == 0)
 		return (0);
-	eol_idx = (int)(ft_strchr(depot[fd], '\n') - depot[fd]);
-	if (*(depot[fd] + eol_idx + 1) == '\0')
+	eol_idx = (int)(ft_strchr(depot_fd, '\n') - depot_fd);
+	if (depot_fd[eol_idx + 1] == '\0')
 	{
-		free(depot[fd]);
+		free(depot_fd);
 		return (0);
 	}
-	trimmed_len = ft_strlen(depot[fd]) - (eol_idx + 1);
-	new_depot = ft_strndup(depot[fd] + eol_idx + 1, trimmed_len);
-	free(depot[fd]);
+	trimmed_len = ft_strlen(depot_fd) - (eol_idx + 1);
+	new_depot = ft_strndup(depot_fd + eol_idx + 1, trimmed_len);
+	free(depot_fd);
 	return (new_depot);
 }
--- GNL/gnl_01/get_next_line_bonus.h
+++ GNL/gnl_01/get_next_line_bonus.h
@@ -22,9 +22,9 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*fill_depot(int fd, char **depot);
-char	*trim_eol(int fd, char **depot);
-char	*trim_depot(int fd, char **depot);
+char	*fill_depot(int fd, char *depot_fd);
+char	*trim_eol(char *depot_fd);
+char	*trim_depot(char *depot_fd);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
