6042551551211a713cd0d9b0d1ffd9edddbcf8fe
jrim <comfortindex@naver.com>
2021-11-30 22:32:53 +0900

    [test] add main for test

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:08:41 by jrim              #+#    #+#             */
-/*   Updated: 2021/11/30 13:53:46 by jrim             ###   ########.fr       */
+/*   Updated: 2021/11/30 22:29:50 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,29 @@ int ft_printf(const char *form, ...);
 int	parse_form(char *form, va_list ap);
 int	detect_type(char type, va_list ap);
 
+int	main(void)
+{
+	int	len;
+
+	len = ft_printf("fxxking ft_printf\n");
+	ft_printf("%d\n", len);
+	len = ft_printf("%u %d\n", 123, -123);
+	ft_printf("%d\n", len);
+	len = ft_printf("%c%c\n", '4', '2');
+	ft_printf("%d\n", len);
+	len = ft_printf("%s %s\n", "(not null)", 0);
+	ft_printf("%d\n", len);
+	len = ft_printf("%x = %X\n", 255, 255);
+	ft_printf("%d\n", len);
+}
+
 int ft_printf(const char *form, ...)
 {
 	va_list ap;
 	int		len;
 
 	va_start(ap, form);
-	len = check_format((char *)form, ap);
+	len = parse_form((char *)form, ap);
 	va_end(ap);
 	return (len);
 }
@@ -36,15 +52,13 @@ int	parse_form(char *form, va_list ap)
 	idx = 0;
 	while (form[idx] != '\0')
 	{
-		while (form[idx] != '%' && form[idx] != '\0')
+		if (form[idx] != '%')
 		{
 			len++;
-			write(1, &form[idx++], 1);
+			write(1, &form[idx], 1);
 		}
-		if (form[idx] == '%')
-			idx++;
-		while (form[idx] != '\0' && ft_strchr(TYPE, form[idx]) != 0)
-			len += detect_type(form[idx], ap);
+		else if (form[idx] == '%' && ft_strchr(TYPE, form[idx + 1]) != 0)
+			len += detect_type(form[++idx], ap);
 		idx++;
 	}
 	return (len);
