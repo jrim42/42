41ffd93d301d67f5d724359e5dc4e139c01660bb
jrim <comfortindex@naver.com>
2021-11-30 20:16:42 +0900

    Backup test codes

--- ft_printf/etc/test.c
+++ ft_printf/etc/test.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int num;
-    char    *str = "42seoul";
+    int         num;
+    long long   ll_num;
+    char    *str;
 
+    str = (char *)calloc(10, sizeof(char));
+    num = printf("%p\n", str);
+    free(str);
+    num = printf("%p\n", str);
+    printf("num : %d\n\n", num);
+    num = printf("%10.d\n", 123);
+    printf("num : %d\n", num);
+    num = printf("%d\n", 2147483648);
+    printf("num : %d\n", num);
     num = printf("%d\n", 123);
     printf("num : %d\n", num);
     num = printf("%%\n");
@@ -17,9 +28,15 @@ int main(void)
     printf("num : %d\n", num);
     num = printf("%%\n");
     printf("num : %d\n", num);
-    printf("%x\n", 2147483647);
+    num = printf("%x\n", 0x123);
+    printf("num : %d\n", num);
     printf("%X\n", 255);
-    while (*str)
-        write(1, str++, 1);
-    printf("\n%\n");
+    printf("%X\n", -2147483647);
+    printf("%X\n", -255);
+    num = -1;
+    printf("%X\n", num);
+    ll_num = num + 4294967295 + 1;
+    printf("%llX\n", ll_num);
+    printf("%d\n", 0XFFFFFF01);
+    printf("%d\n", 0XFFFFFFFF);
 }
