397b716f0a3c6270c3c6de88caaec6f7b44af41d
jrim <comfortindex@naver.com>
2021-12-01 18:20:53 +0900

    [norm]

--- ft_printf/ft_printf_00/ft_printf.c
+++ ft_printf/ft_printf_00/ft_printf.c
@@ -6,19 +6,19 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:08:41 by jrim              #+#    #+#             */
-/*   Updated: 2021/11/30 22:29:50 by jrim             ###   ########.fr       */
+/*   Updated: 2021/12/01 18:14:31 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *form, ...);
+int	ft_printf(const char *form, ...);
 int	parse_form(char *form, va_list ap);
 int	detect_type(char type, va_list ap);
 
-int ft_printf(const char *form, ...)
+int	ft_printf(const char *form, ...)
 {
-	va_list ap;
+	va_list	ap;
 	int		len;
 
 	va_start(ap, form);
@@ -29,8 +29,8 @@ int ft_printf(const char *form, ...)
 
 int	parse_form(char *form, va_list ap)
 {
-	int			len;
-	int			idx;
+	int	len;
+	int	idx;
 
 	len = 0;
 	idx = 0;
@@ -50,20 +50,20 @@ int	parse_form(char *form, va_list ap)
 
 int	detect_type(char type, va_list ap)
 {
-	int		len;
-    
-    len = 0;
-    if (type == '%')
-        len += print_per();
-    else if (type == 'c')
-        len += print_char(ap);
-    else if (type == 's')
-        len += print_str(ap);
-    else if (type == 'x' || type == 'X' || type == 'p')
-        len += print_hex(type, ap);
-    else if (type == 'd' || type == 'i')
-        len += print_int(ap);
-    else if (type == 'u')
-        len += print_uns(ap);
+	int	len;
+
+	len = 0;
+	if (type == '%')
+		len += print_per();
+	else if (type == 'c')
+		len += print_char(ap);
+	else if (type == 's')
+		len += print_str(ap);
+	else if (type == 'x' || type == 'X' || type == 'p')
+		len += print_hex(type, ap);
+	else if (type == 'd' || type == 'i')
+		len += print_int(ap);
+	else if (type == 'u')
+		len += print_uns(ap);
 	return (len);
 }
--- ft_printf/ft_printf_00/ft_printf_str.c
+++ ft_printf/ft_printf_00/ft_printf_str.c
@@ -6,40 +6,40 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 16:17:04 by jrim              #+#    #+#             */
-/*   Updated: 2021/11/30 22:11:18 by jrim             ###   ########.fr       */
+/*   Updated: 2021/12/01 18:20:14 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int print_char(va_list ap);
-int print_str(va_list ap);
-int print_per(void);
+int	print_char(va_list ap);
+int	print_str(va_list ap);
+int	print_per(void);
 
-int print_char(va_list ap)
+int	print_char(va_list ap)
 {
-	int		ch;
+	int	ch;
 
-    ch = va_arg(ap, int);
-    write(1, &ch, 1);
-    return (1);
+	ch = va_arg(ap, int);
+	write(1, &ch, 1);
+	return (1);
 }
 
-int print_str(va_list ap)
+int	print_str(va_list ap)
 {
-    const char  *str;
-    size_t      len;
+	const char	*str;
+	size_t		len;
 
-    str = va_arg(ap, char *);
-    if (!str)
-        str = "(null)";
-    len = ft_strlen(str);
-    write(1, str, len);
-    return (len);
+	str = va_arg(ap, char *);
+	if (!str)
+		str = "(null)";
+	len = ft_strlen(str);
+	write(1, str, len);
+	return (len);
 }
 
-int print_per(void)
+int	print_per(void)
 {
-    write(1, "%", 1);
-    return (1);
+	write(1, "%", 1);
+	return (1);
 }
