0ba2841bfd467dfaf8a1bad04e10cdc6a08b6c22
jrim <comfortindex@naver.com>
2021-12-06 20:41:07 +0900

    [fix] main for flag check

--- ft_printf/field/main_flag.c
+++ ft_printf/field/main_flag.c
@@ -35,19 +35,20 @@ int main(void)
     // // printf("len : %d\n", len);
 
     // printf("------------\n");
-    // printf("|%-10d|\n", 1234);
+    // len = printf("|%-10d|\n", 1234);
+    // printf("len : %d\n", len);
     // printf("|%-3d|\n", 1234);
     // printf("|%10d|\n", 1234);
     // printf("|%3d|\n", 1234);
     // printf("------------\n");
     // len = ft_printf("|%-10d|\n", 1234);
-    // // printf("len : %d\n", len);
+    // printf("len : %d\n", len);
     // len = ft_printf("|%-3d|\n", 1234);
-    // // printf("len : %d\n", len);
+    // printf("len : %d\n", len);
     // len = ft_printf("|%10d|\n", 1234);
-    // // printf("len : %d\n", len);
+    // printf("len : %d\n", len);
     // len = ft_printf("|%3d|\n", 1234);
-    // // printf("len : %d\n", len);
+    // printf("len : %d\n", len);
 
     // printf("------------\n");
     // printf("|%010d|\n", 1234);
@@ -92,23 +93,98 @@ int main(void)
     // printf(" %#x \n", LONG_MIN);
     // printf(" %#x \n", ULONG_MAX);
 
-    // ft_printf(" %#x \n", 0);
-    // ft_printf(" %#x \n", -42);
-    // ft_printf(" %#x \n", INT_MAX);
-    // ft_printf(" %#x \n", INT_MIN);
-    // ft_printf(" %#x \n", LONG_MAX);
-    // ft_printf(" %#x \n", LONG_MIN);
-    // ft_printf(" %#x \n", ULONG_MAX);
+    // len = ft_printf(" %d \n", 0);
+    // // printf("len : %d\n", len);
+    // len = ft_printf(" %d \n", -42);
+    // // printf("len : %d\n", len);
+    // len = ft_printf(" %d \n", INT_MAX);
+    // // printf("len : %d\n", len);
+    // len = ft_printf(" %d \n", INT_MIN);
+    // // printf("len : %d\n", len);
+    // len = ft_printf(" %d \n", LONG_MAX);
+    // // printf("len : %d\n", len);
+    // len = ft_printf(" %d \n", LONG_MIN);
+    // // printf("len : %d\n", len);
+    // len = ft_printf(" %d \n", ULONG_MAX);
+    // // printf("len : %d\n", len);
 
-    printf(" %04d \n", -14);
-    printf(" %05d \n", -15);
-    printf(" %06d \n", -16);
-    printf(" %011d \n", LONG_MAX);
-    ft_printf(" %04d \n", -14);
-    ft_printf(" %05d \n", -15);
-    ft_printf(" %06d \n", -16);
-    ft_printf(" %011d \n", LONG_MAX);
+    // printf(" %04d \n", 0);
+    // printf(" %04d \n", -14);
+    // printf(" %05d \n", -15);
+    // printf(" %06d \n", -16);
+    // printf(" %011d \n", LONG_MAX);
+    // ft_printf(" %04d \n", -14);
+    // ft_printf(" %05d \n", -15);
+    // ft_printf(" %06d \n", -16);
+    // ft_printf(" %011d \n", LONG_MAX);
 
-    printf(" %01d \n", 0);
-    ft_printf(" %01d \n", 0);
+    // printf(" %d \n", LONG_MIN);
+    // ft_printf(" %d %d %d %d %d %d %d\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+
+    // printf(" %x \n", 2147483648);
+    // ft_printf(" %x \n", 2147483648);
+    // printf(" %x \n", -1);
+    // ft_printf(" %x \n", -1);
+    // printf(" %x \n", -2147483649);
+    // ft_printf(" %x \n", -2147483649);
+
+    // printf(" %01d \n", 0);
+    // ft_printf(" %01d \n", 0);
+
+    len = printf("|% d|\n", 0);
+    printf("len : %d\n", len);
+    len = printf("|% d|\n", -1);
+    printf("len : %d\n", len);
+// 	printf("|% d|\n", -9);
+// 	printf("|% d|\n", -10);
+// 	printf("|% d|\n", -11);
+// 	printf("|% d|\n", -15);
+// 	printf("|% d|\n", -16);
+// 	printf("|% d|\n", -17);
+// 	printf("|% d|\n", -99);
+	len = printf("|% d|\n", -100);
+    printf("len : %d\n", len);
+    len = printf("|% d|\n", 100);
+    printf("len : %d\n", len);
+// 	printf("|% d|\n", -101);
+// 	printf("|% d|\n", INT_MAX);
+// 	printf("|% d|\n", LONG_MIN);
+// 	printf("% d % d % d % d % d % d % d\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+
+    len = ft_printf("|% d|\n", 0);
+	printf("len : %d\n", len);
+    len = ft_printf("|% d|\n", -1);
+    printf("len : %d\n", len);
+// 	ft_printf("|% d|\n", -9);
+// 	ft_printf("|% d|\n", -10);
+// 	ft_printf("|% d|\n", -11);
+// 	ft_printf("|% d|\n", -15);
+// 	ft_printf("|% d|\n", -16);
+// 	ft_printf("|% d|\n", -17);
+// 	ft_printf("|% d|\n", -99);
+	len = ft_printf("|% d|\n", -100);
+    printf("len : %d\n", len);
+    len = ft_printf("|% d|\n", 100);
+    printf("len : %d\n", len);
+// 	ft_printf("|% d|\n", -101);
+// 	ft_printf("|% d|\n", INT_MAX);
+// 	ft_printf("|% d|\n", LONG_MIN);
+// 	ft_printf("% d % d % d % d % d % d % d\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+
+    // printf("%+u\n", 0);
+    // printf("%+u\n", 10);
+    // printf("%+u\n", -11);
+    // ft_printf("%+u\n", 0);
+    // ft_printf("%+u\n", 10);
+    // ft_printf("%+u\n", -11);   
+
+    // len = printf("|%-0s|\n", "");
+    // printf("len : %d\n", len);
+    // len = ft_printf("|%-0s|\n", "");
+    // printf("len : %d\n", len);
+
+    printf("%20x|\n", 10);
+    printf("%-20x|\n", 10);
+    ft_printf("%20x|\n", 10);
+    ft_printf("%-20x|\n", 10);
 }
