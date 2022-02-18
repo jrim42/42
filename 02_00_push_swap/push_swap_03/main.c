3eea7f3af1726975639331ec5727fc373a2099d3
jrim <jrim@student.42.fr>
2022-02-18 13:01:24 +0900

    [norm]

--- 02_00_push_swap/push_swap_03/main.c
+++ 02_00_push_swap/push_swap_03/main.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 21:37:22 by jrim              #+#    #+#             */
-/*   Updated: 2022/02/18 12:23:28 by jrim             ###   ########.fr       */
+/*   Updated: 2022/02/18 13:01:06 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int argc, char **argv)
 {
 	push_swap(argc, argv);
-	system("leaks a.out > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
+	system("leaks a.out > leaks_result;\
+			cat leaks_result | grep leaked && rm -rf leaks_result");
 	return (0);
 }
