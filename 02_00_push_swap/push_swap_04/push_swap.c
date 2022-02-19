d45c96c0b405c6eb1ff0bbf8506531595d1dd8a3
jrim <jrim@student.42.fr>
2022-02-19 17:55:59 +0900

    [frx] makefile

--- 02_00_push_swap/push_swap_04/push_swap.c
+++ 02_00_push_swap/push_swap_04/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 21:38:54 by jrim              #+#    #+#             */
-/*   Updated: 2022/02/19 15:23:15 by jrim             ###   ########.fr       */
+/*   Updated: 2022/02/19 17:55:10 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,7 @@ int	main(int argc, char **argv)
 	stk_a2b(elm, elm->piv, elm->a->size);
 	cmd_cleaner(elm);
 	cmd_display_all(elm->cmdlst);
-	stk_display_all(elm->a, elm->b);
 	elm_free(elm);
-	system("leaks a.out > leaks_result;\
-			cat leaks_result | grep leaked && rm -rf leaks_result");
 	return (0);
 }
 
