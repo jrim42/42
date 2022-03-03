f6c4beb7ba84cf2b8eee1c23f181d16ab2b9f888
jrim <comfortindex@naver.com>
2022-03-03 17:04:57 +0900

    [refact]

--- 02_01_minitalk/minitalk_01/_client.c
+++ 02_01_minitalk/minitalk_01/_client.c
@@ -6,40 +6,39 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:21:07 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/03 13:47:18 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/03 17:03:29 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	check_arg(int argc, char **argv);
+void	check_arg(int argc, char **argv, int *srv_pid);
 void	display_clt_err(int err_type);
 void	action(int sig);
 void	send_msg(char *msg, int pid);
 
 int	main(int argc, char **argv)
 {
-	int		pid;
-	char	*msg;
+	int		clt_pid;
+	int		srv_pid;
 
-	check_arg(argc, argv);
-	pid = ft_atoi(argv[1]);
-	ft_putstr_fd("[sent] : ", 1);
-	ft_putnbr_fd(pid, 1);
-	ft_putchar_fd('\n', 1);
-	if (pid <= 100 || pid > 999999)
-		display_clt_err(1);
-	ft_putstr_fd("[received] : ", 1);
+	check_arg(argc, argv, &srv_pid);
+	clt_pid = getpid();
+	ft_putstr_fd("> client pid is [\033[0;33m", 1);
+	ft_putnbr_fd(clt_pid, 1);
+	ft_putstr_fd("\033[0;37m]\n", 1);
+	ft_putstr_fd("> message is sent to [\033[0;33m", 1);
+	ft_putnbr_fd(srv_pid, 1);
+	ft_putstr_fd("\033[0;37m]\n", 1);
 	signal(SIGUSR1, action);
 	signal(SIGUSR2, action);
-	msg = argv[2];
-	send_msg(msg, pid);
+	send_msg(argv[2], srv_pid);
 	while (1)
 		pause();
 	return (0);
 }
 
-void	check_arg(int argc, char **argv)
+void	check_arg(int argc, char **argv, int *srv_pid)
 {
 	int	idx;
 
@@ -49,18 +48,17 @@ void	check_arg(int argc, char **argv)
 	while (argv[1][++idx])
 		if (ft_isdigit(argv[1][idx]) == 0)
 			display_clt_err(1);
+	*srv_pid = ft_atoi(argv[1]);
+	if (*srv_pid <= 100 || *srv_pid > 999999)
+		display_clt_err(1);
 }
 
 void	display_clt_err(int err_type)
 {
 	if (err_type == 0)
-		ft_putstr_fd("[usage] : ./client [server pid] [message to send]", 1);
+		ft_putstr_fd("usage : ./client [server pid] [message to send]\n", 1);
 	else
-		ft_putstr_fd("[error] : ", 1);
-	if (err_type == 1)
-		ft_putendl_fd("invalid pid", 1);
-	else if (err_type == 2)
-		ft_putendl_fd("error type 2", 1);
+		ft_putstr_fd("error : invalid pid number\n", 1);
 	exit(1);
 }
 
@@ -69,11 +67,12 @@ void	action(int sig)
 	static int	received = 0;
 
 	if (sig == SIGUSR1)
-		++received;
+		received++;
 	else
 	{
-		ft_putchar_fd(received, 1);
-		ft_putchar_fd('\n', 1);
+		ft_putstr_fd("> server has received [\033[0;33m", 1);
+		ft_putnbr_fd(received, 1);
+		ft_putstr_fd("\033[0;37m] characters\n", 1);
 		exit(0);
 	}
 }
@@ -96,7 +95,7 @@ void	send_msg(char *msg, int pid)
 			usleep(100);
 		}
 	}
-	bit = 8; // sending null character
+	bit = 8;
 	while (bit--)
 	{
 		kill(pid, SIGUSR1);
--- 02_01_minitalk/minitalk_01/_server.c
+++ 02_01_minitalk/minitalk_01/_server.c
@@ -6,29 +6,28 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:21:10 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/03 13:44:00 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/03 17:02:49 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 void	action(int sig, siginfo_t *info, void *context);
-int		ft_power(int num, int cnt);
 
 int	main(int argc, char **argv)
 {
-	int					pid;
+	int					srv_pid;
 	struct sigaction	s_sigaction;
 
 	if (argc != 1 || argv[1])
 	{
-		ft_putstr_fd("usage : ./server [no argument needed]", 1);
+		ft_putstr_fd("usage : ./server [no argument is needed]\n", 1);
 		return (1);
 	}
-	pid = getpid();
-	ft_putstr_fd("[server pid] : ", 1);
-	ft_putnbr_fd(pid, 1);
-	ft_putchar_fd('\n', 1);
+	srv_pid = getpid();
+	ft_putstr_fd("> server pid is [\033[0;33m", 1);
+	ft_putnbr_fd(srv_pid, 1);
+	ft_putstr_fd("\033[0;37m]\n", 1);
 	s_sigaction.sa_sigaction = action;
 	s_sigaction.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sigaction, NULL);
@@ -42,25 +41,31 @@ void	action(int sig, siginfo_t *info, void *context)
 {
 	static int		idx = 0;
 	static pid_t	clt_pid = 0;
-	static int		ch = 0;
+	static char		ch = 0;
 
 	(void)context;
 	if (clt_pid == 0)
+	{
 		clt_pid = info->si_pid;
+		ft_putstr_fd("> message from [\033[0;33m", 1);
+		ft_putnbr_fd(clt_pid, 1);
+		ft_putstr_fd("\033[0;37m] : ", 1);
+	}
 	ch |= (sig == SIGUSR2);
 	if (++idx == 8)
 	{
 		idx = 0;
 		if (ch == 0)
 		{
-			kill(clt_pid, SIGUSR2);
+			kill(clt_pid, SIGUSR2); //if msg is over, send SIGUSER2 to client
 			clt_pid = 0;
-			ft_putchar_fd('\n', 1);
-			return ;
+			ft_putstr_fd("\n", 1);
+			exit(0);
+			// return ;
 		}
 		ft_putchar_fd(ch, 1);
 		ch = 0;
-		kill(clt_pid, SIGUSR1);
+		kill(clt_pid, SIGUSR1); //if msg isn't over, send SIGUSER1 to client
 	}
 	else
 		ch <<= 1;
