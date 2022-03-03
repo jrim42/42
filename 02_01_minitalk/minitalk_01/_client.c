2f523f6d0cc2f745478a431cfc08dd83e2958797
jrim <comfortindex@naver.com>
2022-03-03 13:47:32 +0900

    [norm]

--- 02_01_minitalk/minitalk_01/_client.c
+++ 02_01_minitalk/minitalk_01/_client.c
@@ -6,100 +6,100 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:21:07 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/03 13:31:25 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/03 13:47:18 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    check_arg(int argc, char **argv);
-void    display_clt_err(int err_type);
-void    action(int sig);
-void    send_msg(char *msg, int pid);
+void	check_arg(int argc, char **argv);
+void	display_clt_err(int err_type);
+void	action(int sig);
+void	send_msg(char *msg, int pid);
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int     pid;
-    char    *msg;
-    
-    check_arg(argc, argv);
-    pid = ft_atoi(argv[1]);
-    ft_putstr_fd("[sent] : ", 1);
-    ft_putnbr_fd(pid, 1);
-    ft_putchar_fd('\n', 1);
-    if (pid <= 100 || pid > 999999)
-        display_clt_err(1);
-    ft_putstr_fd("[received] : ", 1);
-    signal(SIGUSR1, action);
-    signal(SIGUSR2, action);
-    msg = argv[2];
-    send_msg(msg, pid);
-    while (1)
-        pause();
-    return (0);
+	int		pid;
+	char	*msg;
+
+	check_arg(argc, argv);
+	pid = ft_atoi(argv[1]);
+	ft_putstr_fd("[sent] : ", 1);
+	ft_putnbr_fd(pid, 1);
+	ft_putchar_fd('\n', 1);
+	if (pid <= 100 || pid > 999999)
+		display_clt_err(1);
+	ft_putstr_fd("[received] : ", 1);
+	signal(SIGUSR1, action);
+	signal(SIGUSR2, action);
+	msg = argv[2];
+	send_msg(msg, pid);
+	while (1)
+		pause();
+	return (0);
 }
 
-void check_arg(int argc, char **argv)
+void	check_arg(int argc, char **argv)
 {
-    int idx;
+	int	idx;
 
-    if (argc != 3)
-        display_clt_err(0);
-    idx = -1;
-    while (argv[1][++idx])
-        if (ft_isdigit(argv[1][idx]) == 0)
-            display_clt_err(1);
+	if (argc != 3)
+		display_clt_err(0);
+	idx = -1;
+	while (argv[1][++idx])
+		if (ft_isdigit(argv[1][idx]) == 0)
+			display_clt_err(1);
 }
 
-void    display_clt_err(int err_type)
+void	display_clt_err(int err_type)
 {
-    if (err_type == 0)
-        ft_putstr_fd("[usage] : ./client [server pid] [message to send]", 1);
-    else 
-        ft_putstr_fd("[error] : ", 1);
-    if (err_type == 1)
-        ft_putendl_fd("invalid pid", 1);
-    else if (err_type == 2)
-        ft_putendl_fd("error type 2", 1);
-    exit(1);
+	if (err_type == 0)
+		ft_putstr_fd("[usage] : ./client [server pid] [message to send]", 1);
+	else
+		ft_putstr_fd("[error] : ", 1);
+	if (err_type == 1)
+		ft_putendl_fd("invalid pid", 1);
+	else if (err_type == 2)
+		ft_putendl_fd("error type 2", 1);
+	exit(1);
 }
 
-void    action(int sig)
+void	action(int sig)
 {
-    static int  received = 0;
+	static int	received = 0;
 
-    if (sig == SIGUSR1)
-        ++received;
-    else
-    {
-        ft_putchar_fd(received, 1);
-        ft_putchar_fd('\n', 1);
-        exit(0);
-    }
+	if (sig == SIGUSR1)
+		++received;
+	else
+	{
+		ft_putchar_fd(received, 1);
+		ft_putchar_fd('\n', 1);
+		exit(0);
+	}
 }
 
-void    send_msg(char *msg, int pid)
+void	send_msg(char *msg, int pid)
 {
-    int     bit;
-    char    ch;
+	int		bit;
+	char	ch;
 
-    while (*msg)
-    {
-        bit = 8;
-        ch = *msg++;
-        while (bit--)
-        {
-            if (ch >> bit & 1)
-                kill(pid, SIGUSR2);
-            else 
-                kill(pid, SIGUSR1);
-            usleep(100);
-        }
-    }
-    bit = 8; // sending null character
-    while (bit--)
-    {
-        kill(pid, SIGUSR1);
-        usleep(100);
-    }
+	while (*msg)
+	{
+		bit = 8;
+		ch = *msg++;
+		while (bit--)
+		{
+			if (ch >> bit & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	bit = 8; // sending null character
+	while (bit--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
 }
--- 02_01_minitalk/minitalk_01/_server.c
+++ 02_01_minitalk/minitalk_01/_server.c
@@ -6,62 +6,62 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:21:10 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/03 13:33:31 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/03 13:44:00 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    action(int sig, siginfo_t *info, void *context);
-int     ft_power(int num, int cnt);
+void	action(int sig, siginfo_t *info, void *context);
+int		ft_power(int num, int cnt);
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int                 pid;
-    struct sigaction    s_sigaction; 
+	int					pid;
+	struct sigaction	s_sigaction;
 
-    if (argc != 1 || argv[1])
-    {
-        ft_putstr_fd("usage : ./server [no argument needed]", 1);
-        return (1);
-    }
-    pid = getpid();
-    ft_putstr_fd("[server pid] : ", 1);
-    ft_putnbr_fd(pid, 1);
-    ft_putchar_fd('\n', 1);
-    s_sigaction.sa_sigaction = action;
-    s_sigaction.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &s_sigaction, NULL);
-    sigaction(SIGUSR2, &s_sigaction, NULL);
-    while (1)
-        pause();
-    return (0);
+	if (argc != 1 || argv[1])
+	{
+		ft_putstr_fd("usage : ./server [no argument needed]", 1);
+		return (1);
+	}
+	pid = getpid();
+	ft_putstr_fd("[server pid] : ", 1);
+	ft_putnbr_fd(pid, 1);
+	ft_putchar_fd('\n', 1);
+	s_sigaction.sa_sigaction = action;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, NULL);
+	sigaction(SIGUSR2, &s_sigaction, NULL);
+	while (1)
+		pause();
+	return (0);
 }
 
-void    action(int sig, siginfo_t *info, void *context)
+void	action(int sig, siginfo_t *info, void *context)
 {
-    static int      idx = 0;
-    static pid_t    clt_pid = 0;
-    static int      ch = 0;
+	static int		idx = 0;
+	static pid_t	clt_pid = 0;
+	static int		ch = 0;
 
-    (void)context;
-    if (clt_pid == 0)
-        clt_pid = info->si_pid;
-    ch |= (sig == SIGUSR2);
-    if (++idx == 8)
-    {
-        idx = 0;
-        if (ch == 0)
-        {
-            kill(clt_pid, SIGUSR2);
-            clt_pid = 0;
-            ft_putchar_fd('\n', 1);
-            return ;
-        }
-        ft_putchar_fd(ch, 1);
-        ch = 0;
-        kill(clt_pid, SIGUSR1);
-    }
-    else
-        ch <<= 1;
+	(void)context;
+	if (clt_pid == 0)
+		clt_pid = info->si_pid;
+	ch |= (sig == SIGUSR2);
+	if (++idx == 8)
+	{
+		idx = 0;
+		if (ch == 0)
+		{
+			kill(clt_pid, SIGUSR2);
+			clt_pid = 0;
+			ft_putchar_fd('\n', 1);
+			return ;
+		}
+		ft_putchar_fd(ch, 1);
+		ch = 0;
+		kill(clt_pid, SIGUSR1);
+	}
+	else
+		ch <<= 1;
 }
