7d5b940331d239344b1803969ddf2d337c69c0c6
jrim <comfortindex@naver.com>
2022-03-05 12:34:27 +0900

    [refact]

--- 02_01_minitalk/minitalk_01/_client.c
+++ 02_01_minitalk/minitalk_01/_client.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:21:07 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/04 10:08:58 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/05 12:15:57 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ void	recieve_sig(int sig)
 	{
 		ft_putstr_fd("> server has received [\033[0;33m", 1);
 		ft_putnbr_fd(received, 1);
-		ft_putstr_fd("\033[0;37m] characters\n", 1);
+		ft_putstr_fd("\033[0;37m]-character message\n", 1);
 		exit(0);
 	}
 }
@@ -97,7 +97,7 @@ void	check_arg(int argc, char **argv, int *srv_pid)
 		}
 	}
 	*srv_pid = ft_atoi(argv[1]);
-	if (*srv_pid <= 100 || *srv_pid > 999999)
+	if (*srv_pid <= 100 || *srv_pid > 999998)
 	{
 		ft_putstr_fd("error : invalid pid number\n", 1);
 		exit(1);
--- 02_01_minitalk/minitalk_01/_server.c
+++ 02_01_minitalk/minitalk_01/_server.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:21:10 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/04 10:02:11 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/05 12:33:45 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	main(int argc, char **argv)
 
 void	recieve_msg(int sig, siginfo_t *info, void *context)
 {
-	static int		idx = 0;
+	static int		bit = 0;
 	static pid_t	clt_pid = 0;
 	static char		ch = 0;
 
@@ -52,16 +52,15 @@ void	recieve_msg(int sig, siginfo_t *info, void *context)
 		ft_putstr_fd("\033[0;37m] : ", 1);
 	}
 	ch |= (sig == SIGUSR2);
-	if (++idx == 8)
+	if (++bit == 8)
 	{
-		idx = 0;
+		bit = 0;
 		if (ch == 0)
 		{
 			kill(clt_pid, SIGUSR2); //if msg is over, send SIGUSER2 to client
 			clt_pid = 0;
 			ft_putstr_fd("\n", 1);
 			exit(0);
-			// return ;
 		}
 		ft_putchar_fd(ch, 1);
 		ch = 0;
