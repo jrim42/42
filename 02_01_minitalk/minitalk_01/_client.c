cd3827278555c05fcb52f3f5d49e249932575e8a
jrim <comfortindex@naver.com>
2022-03-05 22:56:19 +0900

    [refact] and [norm]

--- 02_01_minitalk/minitalk_01/_client.c
+++ 02_01_minitalk/minitalk_01/_client.c
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:21:07 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/05 12:15:57 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/05 22:50:13 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	send_msg(char *msg, int pid);
 void	recieve_sig(int sig);
 void	check_arg(int argc, char **argv, int *srv_pid);
+void	print_dfl_msg_clt(int type, int num);
 
 int	main(int argc, char **argv)
 {
@@ -23,12 +24,8 @@ int	main(int argc, char **argv)
 
 	check_arg(argc, argv, &srv_pid);
 	clt_pid = getpid();
-	ft_putstr_fd("> client pid is [\033[0;33m", 1);
-	ft_putnbr_fd(clt_pid, 1);
-	ft_putstr_fd("\033[0;37m]\n", 1);
-	ft_putstr_fd("> message is sent to [\033[0;33m", 1);
-	ft_putnbr_fd(srv_pid, 1);
-	ft_putstr_fd("\033[0;37m]\n", 1);
+	print_dfl_msg_clt(0, clt_pid);
+	print_dfl_msg_clt(1, srv_pid);
 	signal(SIGUSR1, recieve_sig);
 	signal(SIGUSR2, recieve_sig);
 	send_msg(argv[2], srv_pid);
@@ -102,4 +99,20 @@ void	check_arg(int argc, char **argv, int *srv_pid)
 		ft_putstr_fd("error : invalid pid number\n", 1);
 		exit(1);
 	}
+}
+
+void	print_dfl_msg_clt(int type, int num)
+{
+	if (type == 0)
+	{
+		ft_putstr_fd("> client pid is [\033[0;33m", 1);
+		ft_putnbr_fd(num, 1);
+		ft_putstr_fd("\033[0;37m]\n", 1);
+	}
+	else if (type == 1)
+	{
+		ft_putstr_fd("> message is sent to [\033[0;33m", 1);
+		ft_putnbr_fd(num, 1);
+		ft_putstr_fd("\033[0;37m]\n", 1);
+	}	
 }
--- 02_01_minitalk/minitalk_01/_server.c
+++ 02_01_minitalk/minitalk_01/_server.c
@@ -6,13 +6,15 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 20:21:10 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/05 12:33:45 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/05 22:54:16 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 void	recieve_msg(int sig, siginfo_t *info, void *context);
+void	print_ch(char ch, int *clt_pid);
+void	print_dfl_msg_srv(int type, int num);
 
 int	main(int argc, char **argv)
 {
@@ -25,9 +27,7 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	srv_pid = getpid();
-	ft_putstr_fd("> server pid is [\033[0;33m", 1);
-	ft_putnbr_fd(srv_pid, 1);
-	ft_putstr_fd("\033[0;37m]\n", 1);
+	print_dfl_msg_srv(0, srv_pid);
 	s_sigaction.sa_sigaction = recieve_msg;
 	s_sigaction.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sigaction, NULL);
@@ -47,25 +47,44 @@ void	recieve_msg(int sig, siginfo_t *info, void *context)
 	if (clt_pid == 0)
 	{
 		clt_pid = info->si_pid;
-		ft_putstr_fd("> message from [\033[0;33m", 1);
-		ft_putnbr_fd(clt_pid, 1);
-		ft_putstr_fd("\033[0;37m] : ", 1);
+		print_dfl_msg_srv(1, clt_pid);
 	}
 	ch |= (sig == SIGUSR2);
 	if (++bit == 8)
 	{
 		bit = 0;
-		if (ch == 0)
-		{
-			kill(clt_pid, SIGUSR2); //if msg is over, send SIGUSER2 to client
-			clt_pid = 0;
-			ft_putstr_fd("\n", 1);
-			exit(0);
-		}
-		ft_putchar_fd(ch, 1);
+		print_ch(ch, &clt_pid);
 		ch = 0;
-		kill(clt_pid, SIGUSR1); //if msg isn't over, send SIGUSER1 to client
 	}
 	else
 		ch <<= 1;
+}
+
+void	print_ch(char ch, int *clt_pid)
+{
+	if (ch == 0)
+	{
+		kill(*clt_pid, SIGUSR2);
+		(*clt_pid) = 0;
+		ft_putstr_fd("\n", 1);
+		exit(0);
+	}
+	ft_putchar_fd(ch, 1);
+	kill(*clt_pid, SIGUSR1);
+}
+
+void	print_dfl_msg_srv(int type, int num)
+{
+	if (type == 0)
+	{
+		ft_putstr_fd("> server pid is [\033[0;33m", 1);
+		ft_putnbr_fd(num, 1);
+		ft_putstr_fd("\033[0;37m]\n", 1);
+	}
+	else if (type == 1)
+	{
+		ft_putstr_fd("> message from [\033[0;33m", 1);
+		ft_putnbr_fd(num, 1);
+		ft_putstr_fd("\033[0;37m] : ", 1);
+	}
 }
