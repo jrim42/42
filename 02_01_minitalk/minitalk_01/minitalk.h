3fb9d23ac3a26cf4bf1a5a4774081d8249d850d2
jrim <comfortindex@naver.com>
2022-03-06 17:13:54 +0900

    [norm] header

--- 02_01_minitalk/minitalk_01/minitalk.h
+++ 02_01_minitalk/minitalk_01/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 21:15:15 by jrim              #+#    #+#             */
-/*   Updated: 2022/03/04 10:04:26 by jrim             ###   ########.fr       */
+/*   Updated: 2022/03/06 12:40:40 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,16 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <signal.h>
+
+// _client.c
+void	send_msg(char *msg, int pid);
+void	recieve_sig(int sig);
+void	check_arg(int argc, char **argv, int *srv_pid);
+void	print_dfl_msg_clt(int type, int num);
+
+// _server.c
+void	recieve_msg(int sig, siginfo_t *info, void *context);
+void	print_ch(char ch, int *clt_pid);
+void	print_dfl_msg_srv(int type, int num);
 
 #endif
