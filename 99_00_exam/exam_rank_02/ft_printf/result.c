3a96135fbfd7d6703f7b4e3cb27ac31fe8659c16
jrim <comfortindex@naver.com>
2022-08-06 00:44:23 +0900

    refact: ft_printf

--- 99_00_exam/exam_rank_02/ft_printf/result.c
+++ 99_00_exam/exam_rank_02/ft_printf/result.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   result.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 21:12:43 by jrim              #+#    #+#             */
-/*   Updated: 2022/08/06 00:26:20 by jrim             ###   ########.fr       */
+/*   Updated: 2022/08/06 00:26:08 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,51 +19,51 @@ int	main(void)
 {
 	int	len;
 
-	len = ft_printf("|10.0s |%10.0s|\n", "Hello");
+	len = printf("|10.0s |%10.0s|\n", "Hello");
 	printf("len: %d\n", len);
-	len = ft_printf("|10.s  |%10.s|\n", "Hello");
+	len = printf("|10.s  |%10.s|\n", "Hello");
 	printf("len: %d\n", len);
-	len = ft_printf("|10s   |%10s|\n", "Hello");
+	len = printf("|10s   |%10s|\n", "Hello");
 	printf("len: %d\n", len);
-	len = ft_printf("|10.4s |%10.4s|\n", "HEllo");
+	len = printf("|10.4s |%10.4s|\n", "HEllo");
 	printf("len: %d\n", len);
-	len = ft_printf("|10.7s |%10.7s|\n", "HEllo");
+	len = printf("|10.7s |%10.7s|\n", "HEllo");
 	printf("len: %d\n", len);
-	len = ft_printf("|.7s   |%.7s|\n", "HEllo");
+	len = printf("|.7s   |%.7s|\n", "HEllo");
 	printf("len: %d\n", len);
-	len = ft_printf("|.3s   |%.3s|\n", "HEllo");
+	len = printf("|.3s   |%.3s|\n", "HEllo");
 	printf("len: %d\n", len);
 
 	printf("-----------------------------------\n");
 
-	len = ft_printf("|d    |%d|\n", 1234);
+	len = printf("|d    |%d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|3d   |%3d|\n", 1234);
+	len = printf("|3d   |%3d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|7d   |%7d|\n", 1234);
+	len = printf("|7d   |%7d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|.d   |%.d|\n", 1234);
+	len = printf("|.d   |%.d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|.3d  |%.3d|\n", 1234);
+	len = printf("|.3d  |%.3d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|.6d  |%.6d|\n", 1234);
+	len = printf("|.6d  |%.6d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|.6d  |%.6d|\n", -1234);
+	len = printf("|.6d  |%.6d|\n", -1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|3.3d |%3.3d|\n", 1234);
+	len = printf("|3.3d |%3.3d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|3.3d |%3.3d|\n", -1234);
+	len = printf("|3.3d |%3.3d|\n", -1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|6.3d |%6.3d|\n", 1234);
+	len = printf("|6.3d |%6.3d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|6.3d |%6.3d|\n", -1234);
+	len = printf("|6.3d |%6.3d|\n", -1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|3.7d |%3.7d|\n", 1234);
+	len = printf("|3.7d |%3.7d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|3.7d |%3.7d|\n", -1234);
+	len = printf("|3.7d |%3.7d|\n", -1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|9.7d |%9.7d|\n", 1234);
+	len = printf("|9.7d |%9.7d|\n", 1234);
 	printf("len: %d\n", len);
-	len = ft_printf("|9.7d |%9.7d|\n", -1234);
+	len = printf("|9.7d |%9.7d|\n", -1234);
 	printf("len: %d\n", len);
 }
