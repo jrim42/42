f1fae72973509b5fc90919c781d1adea933344a6
jrim <comfortindex@naver.com>
2022-08-10 00:43:16 +0900

    backup

---
+++ 03_00_philo/philo_02/philo/_routine.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   _routine.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/31 21:36:13 by jrim              #+#    #+#             */
+/*   Updated: 2022/08/09 22:48:32 by jrim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void		*routine(void *void_philo);
+static void	philo_get_fork(t_philo *philo);
+static void	philo_eat(t_philo *philo);
+static void	philo_sleep(t_philo *philo);
+static void	philo_think(t_philo *philo);
+
+void	*routine(void *void_philo)
+{
+	t_philo	*philo;
+	t_info	*info;
+
+	philo = (t_philo *)void_philo;
+	info = philo->info;
+	gettimeofday(&(philo->last_eat), NULL);
+	while (info->is_done == UNDONE)
+	{
+		philo_get_fork(philo);
+		philo_eat(philo);
+		philo_sleep(philo);
+		philo_think(philo);
+		usleep(100);
+	}
+	return (NULL);
+}
+
+static void	philo_get_fork(t_philo *philo)
+{
+	if (philo->info->is_done == DONE)
+		return ;
+	pthread_mutex_lock(philo->fork_left);
+	pthread_mutex_lock(philo->fork_right);
+	if (philo->info->is_done == DONE)
+	{
+		pthread_mutex_unlock(philo->fork_left);
+		pthread_mutex_unlock(philo->fork_right);
+		return ;
+	}
+	print_routine(philo, "has taken a fork");
+}
+
+static void	philo_eat(t_philo *philo)
+{
+	t_info			*info;
+	struct timeval	start;
+	struct timeval	now;
+
+	info = philo->info;
+	if (info->is_done == DONE)
+		return ;
+	print_routine(philo, "is eating");
+	gettimeofday(&start, NULL);
+	while (1)
+	{
+		gettimeofday(&now, NULL);
+		if (get_time_interval(now, start) >= \
+			(long long)info->param.ms_eat)
+			break ;
+		usleep (10);
+	}
+	pthread_mutex_unlock(philo->fork_left);
+	pthread_mutex_unlock(philo->fork_right);
+	gettimeofday(&philo->last_eat, NULL);
+	philo->eat_cnt++;
+	if (philo->eat_cnt == info->param.n_eat)
+		info->stuffed_philo++;
+}
+
+static void	philo_sleep(t_philo *philo)
+{
+	t_info			*info;
+	struct timeval	start;
+	struct timeval	now;
+
+	info = philo->info;
+	if (info->is_done == DONE)
+		return ;
+	print_routine(philo, "is sleeping");
+	gettimeofday(&start, NULL);
+	while (1)
+	{
+		gettimeofday(&now, NULL);
+		if (get_time_interval(now, start) >= \
+			(long long)info->param.ms_sleep)
+			break ;
+		usleep (10);
+	}
+}
+
+static void	philo_think(t_philo *philo)
+{
+	if (philo->info->is_done == DONE)
+		return ;
+	print_routine(philo, "is thinking");
+}
---
+++ 03_00_philo/philo_02/philo/philo.h
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 20:46:41 by jrim              #+#    #+#             */
+/*   Updated: 2022/08/09 21:44:17 by jrim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+# define NUM "0123456789"
+# define WH_SP " \t\r\v\f\n"
+# define INT_MAX 2147483647
+
+# define SUCCESS 1
+# define FAILURE 0
+# define VALID 1
+# define INVALID 0
+# define DONE 1
+# define UNDONE 0
+
+# define RETURN_ERROR 1
+
+typedef struct s_philo
+{
+	int				name;
+	pthread_t		tid;
+	int				eat_cnt;
+	struct timeval	last_eat;
+	pthread_mutex_t	*fork_left;
+	pthread_mutex_t	*fork_right;
+	struct s_info	*info;
+}					t_philo;
+
+typedef struct s_param
+{
+	int		n_philo;
+	int		ms_die;
+	int		ms_eat;
+	int		ms_sleep;
+	int		n_eat;
+}			t_param;
+
+typedef struct s_info
+{
+	t_philo			*philos;
+	t_param			param;
+	int				stuffed_philo;
+	int				is_done;
+	struct timeval	birthday;
+	pthread_mutex_t	*fork_mtx;
+	pthread_mutex_t	philo_mtx;
+	pthread_mutex_t	msg_mtx;
+}					t_info;
+
+// _philo.c
+int			init_info(t_param *param, t_info *info);
+int			init_philo(t_philo *philo, t_param *param, t_info *info);
+int			create_philo(t_info *info);
+void		bye_philo(t_info *info);
+
+// _param.c
+int			init_param(int argc, char **argv, t_param *param);
+int			_get_param(int argc, char **argv, t_param *param);
+int			philo_atoi(const char *str);
+char		*philo_strchr(const char *s, int c);
+
+// _routine.c
+void		*routine(void *philo_void);
+// static void	philo_get_fork(t_philo *philo);
+// static void	philo_eat(t_philo *philo);
+// static void	philo_sleep(t_philo *philo);
+// static void	philo_think(t_philo *philo);
+
+// _eggshell.c
+void		eggshell(t_info *info);
+
+// _utils.c
+long long	get_time_interval(struct timeval t1, struct timeval t2);
+int			print_error(char *err_msg, int exit_state);
+void		print_routine(t_philo *philo, char *msg);
+
+#endif
