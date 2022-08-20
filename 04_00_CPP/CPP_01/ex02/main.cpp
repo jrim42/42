d66aac80da5593302c36c70b6cd64be44a464ba6
jrim <comfortindex@naver.com>
2022-08-20 20:18:23 +0900

    init: ex02

---
+++ 04_00_CPP/CPP_01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/20 20:14:23 by jrim              #+#    #+#             */
+/*   Updated: 2022/08/20 20:17:39 by jrim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	msg = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &msg;
+	std::string	&stringREF = msg;
+
+	std::cout << "------------------------------" << std::endl;
+	std::cout << "str addr        : " << &msg << std::endl;
+	std::cout << "str addr by ptr : " << stringPTR << std::endl;
+	std::cout << "str addr by ref : " << &stringREF << std::endl;
+	std::cout << "------------------------------" << std::endl;    
+	std::cout << "str by ptr : " << *stringPTR << std::endl;
+	std::cout << "str by ref : " << stringREF << std::endl;
+	std::cout << "------------------------------" << std::endl;    
+
+}
