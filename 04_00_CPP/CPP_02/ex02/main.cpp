2a9286fc00a8c59bff41d9fb19023d2ae4ec61d7
jrim <comfortindex@naver.com>
2022-08-21 17:16:31 +0900

    init: ex02

---
+++ 04_00_CPP/CPP_02/ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/21 16:20:33 by jrim              #+#    #+#             */
+/*   Updated: 2022/08/21 17:15:51 by jrim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) 
+{
+	Fixed		a;
+	Fixed const	b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << "---------------------------------------" << std::endl;
+	std::cout << "  a : " << a << std::endl;
+	std::cout << "++a : " <<  ++a << std::endl;
+	std::cout << "  a : " << a << std::endl;
+	std::cout << "a++ : " << a++ << std::endl;
+	std::cout << "  a : " << a << std::endl;
+	std::cout << "  b : " << b << std::endl;
+	std::cout << "---------------------------------------" << std::endl;
+	std::cout << "max : " << Fixed::max(a, b) << std::endl;
+	std::cout << "min : " << Fixed::min(a, b) << std::endl;
+	std::cout << "---------------------------------------" << std::endl;
+	return (0);
+}
