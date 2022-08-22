5a81679d7fa4213601fb6c34ebe734ca77d9a182
jrim <comfortindex@naver.com>
2022-08-22 12:06:35 +0900

    feat: BSP and main

--- 04_00_CPP/CPP_02/ex03/bsp.cpp
+++ 04_00_CPP/CPP_02/ex03/bsp.cpp
@@ -6,13 +6,57 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 17:25:31 by jrim              #+#    #+#             */
-/*   Updated: 2022/08/21 17:26:02 by jrim             ###   ########.fr       */
+/*   Updated: 2022/08/22 12:00:47 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool	bsp( Point const a, Point const b, Point const c, Point const point)
+bool	check_position(Point const p1, Point const p2, Point const q1, Point const q2);
+Fixed	get_slope(Point const p1, Point const p2);
+
+bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	
+	// m(x - x1) - (y - y1) = 0
+	// mx - y + (-m * x1 + y1) = 0
+	if (check_position(a, b, c, point) == false)
+		return (false);
+	if (check_position(b, c, a, point) == false)
+		return (false);
+	if (check_position(c, a, b, point) == false)
+		return (false);
+	return (true);
+}
+
+bool	check_position(Point const p1, Point const p2, Point const q1, Point const q2)
+{
+	Fixed	m;
+	Fixed	n;
+
+	if (p1.get_x() - p2.get_x() == 0)
+	{
+		if ((q1.get_x() - p1.get_x()) * (q2.get_x() - p1.get_x()) <= 0)
+			return (false);
+	}
+	else
+	{
+		m = get_slope(p1, p2);
+		n = (m * -1 * p1.get_x()) + p1.get_y();
+		if ((m * q1.get_x() - q1.get_y() + n) * (m * q2.get_x() - q2.get_y() + n) <= 0)
+			return (false);
+	}
+	return (true);
+}
+
+Fixed	get_slope(Point const p1, Point const p2)
+{
+	Fixed	denom;
+	Fixed	numer;
+
+	denom = p2.get_x() - p1.get_x();
+	numer = p2.get_y() - p1.get_y();
+	if (numer == 0)
+		return (0);
+	else 
+		return (numer / denom);
 }
--- 04_00_CPP/CPP_02/ex03/main.cpp
+++ 04_00_CPP/CPP_02/ex03/main.cpp
@@ -6,14 +6,39 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 16:20:33 by jrim              #+#    #+#             */
-/*   Updated: 2022/08/21 17:26:19 by jrim             ###   ########.fr       */
+/*   Updated: 2022/08/22 12:04:40 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Point.hpp"
 
+bool	bsp(Point const a, Point const b, Point const c, Point const point);
+
 int main( void ) 
 {
+	Point	a(0, 0);
+	Point	b(20, 0);
+	Point	c(10, 30);
+	Point	p1(10, 15);
+	Point	p2(30, 50);
+	Point	p3(10, 30);
 
+	// case 1 : in
+	if (bsp(a, b, c, p1) == true)
+		std::cout << "p1 is " << BLU << "in" << DFT << " the triangle" << std::endl;
+	else
+		std::cout << "p1 is " << RED << "out" << DFT << " of the triangle" << std::endl;
+	
+	// case 2 : out
+	if (bsp(a, b, c, p2) == true)
+		std::cout << "p2 is " << BLU << "in" << DFT << " the triangle" << std::endl;
+	else
+		std::cout << "p2 is " << RED << "out" << DFT << " of the triangle" << std::endl;
+	
+	// case 3 : out
+	if (bsp(a, b, c, p3) == true)
+		std::cout << "p3 is " << BLU << "in" << DFT << " the triangle" << std::endl;
+	else
+		std::cout << "p3 is " << RED << "out" << DFT << " of the triangle" << std::endl;
 }
