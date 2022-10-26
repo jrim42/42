155dadb7ee957b5abdb119242601fe11eff477ba
Jisu Rim <jrim@c8r6s10.42seoul.kr>
2022-10-26 18:20:11 +0900

    fix: ex00

--- 04_00_CPP/CPP_00/ex00/megaphone.cpp
+++ 04_00_CPP/CPP_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 17:30:54 by jrim              #+#    #+#             */
-/*   Updated: 2022/08/19 17:36:50 by jrim             ###   ########.fr       */
+/*   Updated: 2022/10/26 18:08:25 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<  std::endl;
 	else
 	{
-		for (int i = 0; i < argc; i++)
-			for (int j = 0; i < argc; i++)
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; argv[i][j]; j++)
 				std::cout << static_cast<char>(std::toupper(argv[i][j]));
 		std::cout << std::endl;
 	}
