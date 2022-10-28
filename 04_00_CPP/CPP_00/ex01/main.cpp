9f559b789bb0f266322559828578607e3f84de75
Jisu Rim <jrim@c1r6s3.42seoul.kr>
2022-10-28 22:29:14 +0900

    backup'

--- 04_00_CPP/CPP_00/ex01/main.cpp
+++ 04_00_CPP/CPP_00/ex01/main.cpp
@@ -6,11 +6,10 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 14:41:22 by jrim              #+#    #+#             */
-/*   Updated: 2022/10/26 19:51:28 by jrim             ###   ########.fr       */
+/*   Updated: 2022/10/28 22:28:53 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "contact.hpp"
 #include "phonebook.hpp"
 
 int	main(void)
@@ -31,6 +30,8 @@ int	main(void)
 			phonebook.add_contact();
 		else if (cmd == "SEARCH")
 			phonebook.search_contact();
+		else
+			std::cout << "wrong command. try again." << std::endl;
 	}
 	return (0);
 }
