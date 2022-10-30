1e888f4976125c61bfacd25dae7fa1ef7242730e
jrim <comfortindex@naver.com>
2022-10-31 00:39:10 +0900

    change instance name and some unnecessary changes

--- 04_00_CPP/CPP_01/ex06/Harl.hpp
+++ 04_00_CPP/CPP_01/ex06/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 22:15:02 by jrim              #+#    #+#             */
-/*   Updated: 2022/08/20 22:59:27 by jrim             ###   ########.fr       */
+/*   Updated: 2022/10/31 00:35:14 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,32 @@
 # define RED "\033[0;31m"
 # define GRN "\033[0;32m"
 # define YLW "\033[0;33m"
-# define BLU "\033[0;36m"
+# define BLU "\033[0;34m"
 # define DFT "\033[0;37m"
-# define GRY "\033[0;30m"
 
 class Harl 
 {
-	typedef struct	s_level 
+	typedef struct	s_complain 
 	{
-		std::string	name;
-		void	(Harl::*f_ptr)(void);
-	}				t_level;
+		std::string	level;
+		void		(Harl::*f_ptr)(void);
+	}				t_complain;
 
 	private:
-		t_level	level[4];
-		void	debug(void);
-		void	info(void);
-		void	warning(void);
-		void	error(void);
+		t_complain	complains[4];
+		void		debug(void);
+		void		info(void);
+		void		warning(void);
+		void		error(void);
 		
 	
 	public:
 		Harl(void);
 		~Harl(void);
+		
 		void	init_harl(void);
 		void	complain(std::string level);
 		void	something_else(void);
-
 };
 
 #endif
