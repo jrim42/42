31c9891ea7e8b6227292b4c2aa625d6384d9c8e1
jrim <comfortindex@naver.com>
2022-11-22 17:05:00 +0900

    edit some

--- 04_00_CPP/CPP_02/ex01/Fixed.cpp
+++ 04_00_CPP/CPP_02/ex01/Fixed.cpp
@@ -6,50 +6,52 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 16:24:54 by jrim              #+#    #+#             */
-/*   Updated: 2022/08/21 16:49:26 by jrim             ###   ########.fr       */
+/*   Updated: 2022/11/22 17:04:32 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+//-------------- orthodox canonical form ----------------//
 Fixed::Fixed(void)
 {
-	std::cout << "Default constructor called" << std::endl;
+	std::cout << GRY << "Default constructor called" << DFT << std::endl;
 	this->value = 0;
 }
 
-Fixed::Fixed(const int value)
+Fixed::Fixed(int num)
 {
-	std::cout << GRN << "Int" << DFT << " constructor called" << std::endl;
-	Fixed::value = value << bits;
-}
+	std::cout << BLU << "Int" << DFT << " constructor called" << std::endl;
+	this->value = num << this->bits;}
 
 Fixed::Fixed(const float value)
 {
 	std::cout << BLU << "Float" << DFT << " constructor called" << std::endl;
-	Fixed::value = roundf(value * (1 << bits));
+	this->value = roundf(value * (1 << this->bits));
 }
 
 Fixed::Fixed(const Fixed &ref)
 {
-	std::cout << "Copy constructor called" << std::endl;
-	(*this) = ref;
+	std::cout << GRY << "Copy constructor called" << DFT << std::endl;
+	this->value = ref.getRawBits();
 }
 
-Fixed &Fixed::operator=(Fixed const &ref)
+Fixed& Fixed::operator=(Fixed const &ref)
 {
-	std::cout << RED << "Assignation" << DFT << " operation called" << std::endl;
-	value = ref.getRawBits();
+	std::cout << GRY << "Copy assignment operator called" << DFT << std::endl;
+	this->value = ref.getRawBits();
 	return (*this);
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << GRY << "Destructor called" << DFT << std::endl;
 }
 
+//--------------------- getter/setter -------------------//
 int Fixed::getRawBits(void) const
 {
+	std::cout << GRY << "getRawBits member function called" << DFT << std::endl;
 	return (this->value);
 }
 
@@ -58,6 +60,7 @@ void	Fixed::setRawBits(int const raw)
 	this->value = raw;
 }
 
+//---------------------- converter ----------------------//
 int 	Fixed::toInt(void) const
 {
 	return (value >> bits);
@@ -68,9 +71,10 @@ float	Fixed::toFloat(void) const
 	return (float(value) / (1 << bits));
 }
 
-std::ostream & operator<<(std::ostream &out, const Fixed &fixed)
+//------------------- insertion operator ----------------//
+std::ostream& operator<<(std::ostream &out, const Fixed &fixed)
 {
-	std::cout << GRY << "(ostream operator called) " << DFT;
+	// std::cout << GRY << "(insertion operator called) " << DFT;
 	out << fixed.toFloat();
 	return (out);
 }
--- 04_00_CPP/CPP_02/ex01/Fixed.hpp
+++ 04_00_CPP/CPP_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 16:21:39 by jrim              #+#    #+#             */
-/*   Updated: 2022/11/22 15:41:08 by jrim             ###   ########.fr       */
+/*   Updated: 2022/11/22 17:04:43 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ class Fixed
 		Fixed(const int fixed);					
 		Fixed(const float fixed);					
 		Fixed(const Fixed &ref);				
-		Fixed &	operator=(Fixed const &ref);	
+		Fixed&	operator=(Fixed const &ref);	
 		~Fixed(void);						
 
 		int		getRawBits(void) const;			
@@ -44,6 +44,6 @@ class Fixed
 		float	toFloat(void) const;
 };
 
-std::ostream & operator<<(std::ostream &out, const Fixed &fixed);
+std::ostream& operator<<(std::ostream &out, const Fixed &fixed);
 
 #endif
