b850532a29096b70c95ec709bf9f5ad7454896e4
jrim <comfortindex@naver.com>
2022-12-23 16:38:38 +0900

    add: increaseGrade, decreaseGrade, main test case

--- 04_00_CPP/CPP_05/ex00/Bureaucrat.cpp
+++ 04_00_CPP/CPP_05/ex00/Bureaucrat.cpp
@@ -58,6 +58,29 @@ int	Bureaucrat::getGrade(void) const
 	return (this->_grade);
 }
 
+//----------------------- grades ------------------------//
+void  Bureaucrat::increaseGrade(void)
+{
+	if (this->_grade > 1)
+	{
+		this->_grade -= 1;
+		std::cout << GRY << "(Bureaucrat: increaseGrade)" << DFT << std::endl;
+	}
+	else
+		throw GradeTooHighException();
+}
+
+void  Bureaucrat::decreaseGrade(void)
+{
+	if (this->_grade < 150)
+	{
+		this->_grade += 1;
+		std::cout << GRY << "(Bureaucrat: decreaseGrade)" << DFT << std::endl;
+	}
+	else
+		throw GradeTooLowException();
+}
+
 //---------------------- exception ----------------------//
 const char *Bureaucrat::GradeTooHighException::what(void) const throw()
 {
@@ -73,6 +96,6 @@ const char *Bureaucrat::GradeTooLowException::what(void) const throw()
 std::ostream& operator<<(std::ostream& out, const Bureaucrat& b)
 {
 	out	<< b.getName() << ", bureaucrat grade " 
-		<< BLU << b.getGrade() << DFT << "." << std::endl;
+		<< b.getGrade() << "." << std::endl;
 	return (out);
 }
--- 04_00_CPP/CPP_05/ex00/main.cpp
+++ 04_00_CPP/CPP_05/ex00/main.cpp
@@ -6,21 +6,44 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 12:41:44 by jrim              #+#    #+#             */
-/*   Updated: 2022/11/28 12:41:46 by jrim             ###   ########.fr       */
+/*   Updated: 2022/12/23 16:37:15 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+#define CONTOUR "---------------------------------------" 
+
 int main(void)
 {
-	try
 	{
-		Bureaucrat test("test", 151);
+		try
+		{
+			Bureaucrat test("test", 151);
+		}
+		catch (std::exception& e)
+		{
+			std::cout << RED << e.what() << DFT << std::endl;
+		}
 	}
-	catch (std::exception& e)
+	std::cout << CONTOUR << std::endl;
 	{
-		std::cout << e.what() << std::endl;
+		Bureaucrat	a("a", 3);
+		int			cnt = 3;
+		
+		while (cnt--)
+		{
+			std::cout << a;
+			try
+			{
+				a.increaseGrade();
+			}
+			catch (std::exception& e)
+			{
+				std::cout << RED << e.what() << DFT << std::endl;
+			}
+		}
 	}
 	return (0);
 }
+
