70c047f6588947395fe52dffb7616da63267bd4f
jrim <comfortindex@naver.com>
2022-12-25 16:28:35 +0900

    backup

--- 04_00_CPP/CPP_05/ex02/main.cpp
+++ 04_00_CPP/CPP_05/ex02/main.cpp
@@ -6,9 +6,17 @@
 /*   By: jrim <jrim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 22:40:47 by jrim              #+#    #+#             */
-/*   Updated: 2022/12/21 22:40:59 by jrim             ###   ########.fr       */
+/*   Updated: 2022/12/25 16:28:02 by jrim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+#define CONTOUR "---------------------------------------" 
 
 int main(void)
 {
