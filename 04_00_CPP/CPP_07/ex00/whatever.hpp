04c6f6c44868444f255cbae4fc3bedcb4815a4ce
jrim <comfortindex@naver.com>
2022-12-30 22:16:09 +0900

    add color macro

--- 04_00_CPP/CPP_07/ex00/whatever.hpp
+++ 04_00_CPP/CPP_07/ex00/whatever.hpp
@@ -4,6 +4,13 @@
 # include <iostream>
 # include <string>
 
+# define RED "\033[0;31m"
+# define GRN "\033[0;32m"
+# define YLW "\033[0;33m"
+# define BLU "\033[0;36m"
+# define GRY "\033[90m"
+# define DFT "\033[0;37m"
+
 template<typename T>
 void	swap(T& a, T& b)
 {
