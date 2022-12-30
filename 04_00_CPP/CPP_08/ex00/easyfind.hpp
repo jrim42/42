2d7ee2860a907f763e7d93cd37c5d1b513b9dee6
jrim <comfortindex@naver.com>
2022-12-30 23:07:44 +0900

    output check

--- 04_00_CPP/CPP_08/ex00/easyfind.hpp
+++ 04_00_CPP/CPP_08/ex00/easyfind.hpp
@@ -2,21 +2,27 @@
 # define EASYFIND_HPP
 
 # include <iostream>
+# include <algorithm>
 # include <list>
 # include <deque>
 # include <vector>
 
+# define RED "\033[0;31m"
+# define GRN "\033[0;32m"
+# define YLW "\033[0;33m"
+# define BLU "\033[0;36m"
+# define GRY "\033[90m"
+# define DFT "\033[0;37m"
+
 template <typename T>
-typename T::iterator easyfind(T& container, int value)
+typename T::iterator	easyfind(T& container, int value)
 {
 	typename T::iterator iter;
 
 	iter = std::find(container.begin(), container.end(), value);
 	if (iter == container.end()) 
-	{
-		throw std::runtime_error("value is not in this container");
-	}
-	return iter;
+		throw std::runtime_error("Error: value not found in container");
+	return (iter);
 }
 
 #endif
--- 04_00_CPP/CPP_08/ex00/main.cpp
+++ 04_00_CPP/CPP_08/ex00/main.cpp
@@ -2,36 +2,44 @@
 
 int main(void)
 {
-	std::vector<int> v;
-	std::deque<int> d;
 	std::list<int> l;
+	std::deque<int> d;
+	std::vector<int> v;
 
 	for (int i = 0; i < 10; i++)
 	{
-		v.push_back(i);
-		d.push_back(i);
 		l.push_back(i);
+		d.push_back(i);
+		v.push_back(i);
 	}
 
+	std::cout << GRY << "(case 1: list) " << DFT;
 	try
 	{
-		std::cout << *(easyfind(v, 5)) << " is at " << std::distance(v.begin(), easyfind(v, 5)) << std::endl;
+		std::cout 	<< *(easyfind(l, 8)) << " is found at index " 
+					<< std::distance(l.begin(), easyfind(l, 8)) << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
+
+	std::cout << GRY << "(case 2: deque) " << DFT;
 	try
 	{
-		std::cout << *(easyfind(d, 0)) << " is at " << std::distance(d.begin(), easyfind(d, 5)) << std::endl;
+		std::cout 	<< *(easyfind(d, 6)) << " is found at index " 
+					<< std::distance(d.begin(), easyfind(d, 6)) << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
+	
+	std::cout << GRY << "(case 3: vector) " << DFT;
 	try
 	{
-		std::cout << *(easyfind(l, 10)) << " is at " << std::distance(l.begin(), easyfind(l, 5)) << std::endl;
+		std::cout	<< *(easyfind(v, 11)) << " is found at index " 
+					<< std::distance(v.begin(), easyfind(v, 11)) << std::endl;
 	}
 	catch(const std::exception& e)
 	{
