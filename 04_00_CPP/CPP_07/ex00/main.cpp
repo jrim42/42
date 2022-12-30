f2ebc39f054c5b1f7628c0ca773c61038e3fc890
jrim <comfortindex@naver.com>
2022-12-30 15:49:42 +0900

    draft

--- 04_00_CPP/CPP_07/ex00/main.cpp
+++ 04_00_CPP/CPP_07/ex00/main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include "whatever.hpp"
 
-int main( void ) {
+int main(void) 
+{
     int a = 2;
     int b = 3;
     ::swap( a, b );
---
+++ 04_00_CPP/CPP_07/ex01/iter.hpp
@@ -0,0 +1,29 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <cstddef>
+# include <iostream>
+# include <string>
+
+template<typename T>
+void iter (T *array, size_t length, void (*fn)(T&))
+{
+	for (size_t i = 0; i < length; i++)
+	{
+		fn(array[i]);
+	}
+}
+
+template<typename T>
+void print(T &str)
+{
+	std::cout << str << " ";
+}
+
+template<typename T>
+void sqrt(T &num)
+{
+	num = num * num;
+}
+
+#endif
