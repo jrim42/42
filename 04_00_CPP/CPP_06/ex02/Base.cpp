c29bfd728e6d079e2d2b4a53f467c69a38fb064c
jrim <comfortindex@naver.com>
2022-12-30 15:14:33 +0900

    draft

---
+++ 04_00_CPP/CPP_06/ex02/Base.cpp
@@ -0,0 +1,60 @@
+#include "Base.hpp"
+
+//-------------------------------------------------------//
+Base::~Base(void)
+{
+	// std::cout << GRY << "(Base: destructor)" << DFT << std::endl;
+}
+
+
+//----------------------- utils -------------------------//
+Base* generate(void) 
+{
+	int opt = static_cast<int>(Random::randr(0.0, 10.0)) / 3;
+	switch (opt) 
+	{
+		case 0:
+			std::cout << "Answer is A" << std::endl;
+			return new A();
+		case 1:
+			std::cout << "Answer is B" << std::endl;
+			return new B();
+		case 2:
+			std::cout << "Answer is C" << std::endl;
+			return new C();
+		default:
+			return NULL;
+  	}
+}
+
+void identify(Base* p) 
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+static bool try_cast(const Base& p, const std::string& s) 
+{
+	try 
+	{
+		if (s == "A")
+			(void)dynamic_cast<const A&>(p);
+		else if (s == "B")
+			(void)dynamic_cast<const B&>(p);
+		else if (s == "C")
+			(void)dynamic_cast<const C&>(p);
+		std::cout << s << std::endl;
+	} 
+	catch (std::exception&) {}
+	return false;
+}
+
+void identify(const Base& p) 
+{
+	if (try_cast(p, "A") || try_cast(p, "B") || try_cast(p, "C"))
+    	return;
+}
---
+++ 04_00_CPP/CPP_06/ex02/main.cpp
@@ -0,0 +1,14 @@
+#include "Base.hpp"
+
+int main(void) 
+{
+    for (int i = 0 ; i < 10 ; ++i) 
+    {
+        Base* base = generate();
+        identify(base);
+        identify(*base);
+        std::cout << std::endl;
+        delete base;
+    }
+    return (0);
+}
