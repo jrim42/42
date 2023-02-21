3424b867a3afe8b87be1349e794db8be9239a94d
jisu rim <comfortindex@naver.com>
2023-02-21 15:18:26 +0900

    [refact] compare - finish

--- 05_00_ft_container/01/include/compare.hpp
+++ 05_00_ft_container/01/include/compare.hpp
@@ -6,41 +6,42 @@
 
 namespace ft 
 {
-    // equal
-    template <class iIter1, class iIter2>
-    bool equal (iIter1 first1, iIter1 last1, iIter2 first2) 
-    {
-        while (first1 != last1) 
-        {
+	// equal: 두 범위의 요소가 동일한지 검사하는 함수
+		// [first1, last1] 범위의 요소를 first2부터 시작하는 범위의 요소와 비교하고
+		// 두 범위의 모든 요소가 일치하면 true를 반환한다.
+	template <class iIter1, class iIter2>
+	bool	equal(iIter1 first1, iIter1 last1, iIter2 first2) 
+	{
+		while (first1 != last1) 
+		{
 			if (!(*first1 == *first2))
 				return false;
 			++first1;
 			++first2;
 		}
 		return true;
-    }
+	}
 
-    template <class iIter1, class iIter2, class BinaryPredicate>
-    bool equal (iIter1 first1, iIter1 last1, iIter2 first2, BinaryPredicate pred) 
-    {
-        while (first1 != last1) 
-        {
-			if (!pred(*first1,*first2))
+	template <class iIter1, class iIter2, class BinaryPredicate>
+	bool	equal(iIter1 first1, iIter1 last1, iIter2 first2, BinaryPredicate pred) 
+	{
+		while (first1 != last1) 
+		{
+			if (!pred(*first1, *first2))
 				return false;
 			++first1;
 			++first2;
 		}
 		return true;
-    }
+	}
 
-    // lexicographical_compare
-    template <class iIter1, class iIter2>
-    bool lexicographical_compare
-        (iIter1 first1, iIter1 last1,
-         iIter2 first2, iIter2 last2) 
-    {
-        while (first1 != last1) 
-        {
+	// lexicographical_compare: 사전식 순서로 비교해주는 함수
+		// [first1, last1]이 [first2, last2]보다 사전순으로 작을 경우 true를 반환한다.
+	template <class iIter1, class iIter2>
+	bool	lexicographical_compare(iIter1 first1, iIter1 last1, iIter2 first2, iIter2 last2) 
+	{
+		while (first1 != last1) 
+		{
 			if (first2 == last2 || *first2 < *first1)
 				return false;
 			else if (*first1 < *first2)
@@ -48,23 +49,23 @@ namespace ft
 			++first1;
 			++first2;
 		}
-	    return (first2 != last2);
-    }
+		return (first2 != last2);
+	}
 
-    template <class iIter1, class iIter2, class Compare>
-    bool lexicographical_compare
-        (iIter1 first1, iIter1 last1,
-         iIter2 first2, iIter2 last2, Compare comp) 
-    {
-        while (first1 != last1 && !comp(first1, first2)) 
-        {
-			if (first2 == last2)
+	template <class iIter1, class iIter2, class Compare>
+	bool	lexicographical_compare(iIter1 first1, iIter1 last1, iIter2 first2, iIter2 last2, Compare comp) 
+	{
+		while (first1 != last1) 
+		{
+			if (first2 == last2 || comp(*first1, *first2))
 				return false;
+			else if (comp(*first1, *first2))
+				return true;
 			++first1;
 			++first2;
 		}
-		return first2 != last2 ? false : first1 == last1 || comp(first1, first2);
-    }
+		return (first2 != last2);
+	}
 }  // namespace ft
 
 #endif
