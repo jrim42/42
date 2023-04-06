87c5ccc44b9478ab7f6f08c95c4cf82781662667
jisu rim <comfortindex@naver.com>
2023-04-06 12:03:19 +0900

    init: exam05

---
+++ 99_00_exam/exam_rank_05/00/Warlock.hpp
@@ -0,0 +1,29 @@
+#ifndef WARLOCK_HPP
+# define WARLOCK_HPP
+// #pragma once
+
+#include <iostream>
+
+class Warlock
+{
+    private:
+        std::string name;
+        std::string title;
+
+        Warlock();
+        Warlock(const Warlock &other);
+
+        Warlock &operator=(const Warlock &other);
+
+    public:
+        Warlock(const std::string &name, const std::string &title);
+        ~Warlock();
+
+        const std::string   &getName() const;
+        const std::string   &getTitle() const;
+        void                setTitle(const std::string &title);
+        
+        void    introduce() const;
+};
+
+#endif
